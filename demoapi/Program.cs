80657e64c2d5606d86bc84d2cdd472433b4d89b0
Dave Zimmer <dave.zimmer@viawest.com>
2017-10-09 10:02:47 -0500

    Updated to add in Dockerfile and change port # app is served over

--- demoapi/Program.cs
+++ demoapi/Program.cs
@@ -20,6 +20,7 @@ namespace demoapi
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                   .UseUrls("http://localhost:5010")
                 .Build();
     }
 }
